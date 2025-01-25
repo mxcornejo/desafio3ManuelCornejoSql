faaa57bd65e58e71567753f291e6c98017276b49
Manuel Cornejo <manucornejo@Manu-Cornejo-2.local>
2025-01-25 17:24:13 -0300

    first commit

--- desafio3ManuelCornejo.sql
+++ desafio3ManuelCornejo.sql
@@ -1,12 +1,12 @@
--- 1. Creación de la base de datos
+-- Creación de la base de datos
 CREATE DATABASE desafio3_manuel_cornejo_789;
 
--- Usar la base de datos
+-- Entrar a la base de datos
 \c desafio3_manuel_cornejo_789;
 
--- 2. Creación de las tablas
+-- Creación de las tablas
 
--- Crear la tabla de usuarios
+-- Usuarios
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255),
@@ -15,7 +15,7 @@ CREATE TABLE usuarios (
     rol VARCHAR(20)
 );
 
--- Crear la tabla de posts
+-- Posts
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(255),
@@ -26,7 +26,7 @@ CREATE TABLE posts (
     usuario_id BIGINT REFERENCES usuarios(id)
 );
 
--- Crear la tabla de comentarios
+-- Comentarios
 CREATE TABLE comentarios (
     id SERIAL PRIMARY KEY,
     contenido TEXT,
@@ -35,9 +35,9 @@ CREATE TABLE comentarios (
     post_id BIGINT REFERENCES posts(id)
 );
 
--- 3. Insertar los datos en las tablas
+-- Insertar los datos
 
--- Insertar usuarios
+-- Usuarios
 INSERT INTO usuarios(id, email, nombre, apellido, rol) VALUES
 (DEFAULT, 'juan@mail.com', 'juan', 'perez', 'administrador'),
 (DEFAULT, 'diego@mail.com', 'diego', 'munoz', 'usuario'),
@@ -45,7 +45,7 @@ INSERT INTO usuarios(id, email, nombre, apellido, rol) VALUES
 (DEFAULT, 'roxana@mail.com', 'roxana', 'diaz', 'usuario'),
 (DEFAULT, 'pedro@mail.com', 'pedro', 'diaz', 'usuario');
 
--- Insertar posts
+-- Posts
 INSERT INTO posts(id, titulo, contenido, fecha_creacion, fecha_actualizacion, destacado, usuario_id) VALUES
 (DEFAULT, 'prueba', 'contenido prueba', '2021-01-01', '2021-02-01', true, 1),
 (DEFAULT, 'prueba2', 'contenido prueba2', '2021-03-01', '2021-03-01', true, 1),
@@ -53,7 +53,7 @@ INSERT INTO posts(id, titulo, contenido, fecha_creacion, fecha_actualizacion, de
 (DEFAULT, 'ejercicios2', 'contenido ejercicios2', '2021-05-03', '2021-04-04', false, 2),
 (DEFAULT, 'random', 'contenido random', '2021-06-03', '2021-05-04', false, NULL);
 
--- Insertar comentarios
+-- Comentarios
 INSERT INTO comentarios(id, contenido, fecha_creacion, usuario_id, post_id) VALUES
 (DEFAULT, 'comentario 1', '2021-06-03', 1, 1),
 (DEFAULT, 'comentario 2', '2021-06-03', 2, 1),
@@ -61,27 +61,26 @@ INSERT INTO comentarios(id, contenido, fecha_creacion, usuario_id, post_id) VALU
 (DEFAULT, 'comentario 4', '2021-06-04', 1, 2),
 (DEFAULT, 'comentario 5', '2021-06-04', 2, 2);
 
--- 4. Consultas requeridas
+-- Consultas
 
--- 2. Cruza los datos de la tabla usuarios y posts, mostrando las siguientes columnas:
--- nombre y email del usuario junto al título y contenido del post.
+-- Cruza los datos de la tabla usuarios y posts, mostrando las siguientes columnas: nombre y email del usuario junto al título y contenido del post.
 SELECT u.nombre, u.email, p.titulo, p.contenido
 FROM usuarios u
 JOIN posts p ON u.id = p.usuario_id;
 
--- 3. Muestra el id, título y contenido de los posts de los administradores.
+-- Muestra el id, título y contenido de los posts de los administradores
 SELECT p.id, p.titulo, p.contenido
 FROM posts p
 JOIN usuarios u ON p.usuario_id = u.id
 WHERE u.rol = 'administrador';
 
--- 4. Cuenta la cantidad de posts de cada usuario.
+-- Cuenta la cantidad de posts de cada usuario.
 SELECT u.id, u.email, COUNT(p.id) AS cantidad_posts
 FROM usuarios u
 LEFT JOIN posts p ON u.id = p.usuario_id
 GROUP BY u.id, u.email;
 
--- 5. Muestra el email del usuario que ha creado más posts.
+-- Muestra el email del usuario que ha creado más posts.
 SELECT u.email
 FROM usuarios u
 JOIN posts p ON u.id = p.usuario_id
@@ -89,13 +88,13 @@ GROUP BY u.email
 ORDER BY COUNT(p.id) DESC
 LIMIT 1;
 
--- 6. Muestra la fecha del último post de cada usuario.
+--  Muestra la fecha del último post de cada usuario.
 SELECT u.email, MAX(p.fecha_creacion) AS ultimo_post
 FROM usuarios u
 JOIN posts p ON u.id = p.usuario_id
 GROUP BY u.email;
 
--- 7. Muestra el título y contenido del post con más comentarios.
+-- Muestra el título y contenido del post (artículo) con más comentarios
 SELECT p.titulo, p.contenido
 FROM posts p
 JOIN comentarios c ON p.id = c.post_id
@@ -103,19 +102,19 @@ GROUP BY p.id
 ORDER BY COUNT(c.id) DESC
 LIMIT 1;
 
--- 8. Muestra el título de cada post, el contenido de cada post y el contenido de cada comentario asociado a los posts, junto con el email del usuario que lo escribió.
+-- Muestra en una tabla el título de cada post, el contenido de cada post y el contenido de cada comentario asociado a los posts mostrados, junto con el email del usuario que lo escribió.
 SELECT p.titulo, p.contenido AS post_contenido, c.contenido AS comentario_contenido, u.email
 FROM posts p
 JOIN comentarios c ON p.id = c.post_id
 JOIN usuarios u ON c.usuario_id = u.id;
 
--- 9. Muestra el contenido del último comentario de cada usuario.
+-- Muestra el contenido del último comentario de cada usuario.
 SELECT u.email, c.contenido AS ultimo_comentario
 FROM usuarios u
 JOIN comentarios c ON u.id = c.usuario_id
 WHERE c.fecha_creacion = (SELECT MAX(fecha_creacion) FROM comentarios WHERE usuario_id = u.id);
 
--- 10. Muestra los emails de los usuarios que no han escrito ningún comentario.
+-- Muestra los emails de los usuarios que no han escrito ningún comentario.
 SELECT u.email
 FROM usuarios u
 LEFT JOIN comentarios c ON u.id = c.usuario_id
